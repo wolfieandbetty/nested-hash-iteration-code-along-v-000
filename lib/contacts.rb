fad38301a974a0d1b3f08b173f3e096b78750f69
Cat Norris <cannenorris@yahoo.com>
2018-01-10 04:15:43 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,8 +15,8 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-contacts.each do | person, contact_details_hash |
-  contact_details_hash.each do |attribute, data|
+contacts.each do |person, contact_details_hash|
     binding.pry
   
+end
 end
